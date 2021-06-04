8399674082edaa33a8be484256452711b421f789
Miguel Morais <miguelmoraisdev@gmail.com>
2021-06-03 23:11:44 -0300

    Backend project created

---
+++ backend/src/test/java/com/miguelm/crudcliente/CrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelm.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
