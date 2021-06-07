85d02760ebf3fd52209a4597b001a88c722c31a1
Miguel Morais <miguelmoraisdev@gmail.com>
2021-06-06 23:18:00 -0300

    Delete and DatabaseException

--- backend/src/main/java/com/miguelm/crudcliente/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/miguelm/crudcliente/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.miguelm.crudcliente.services.exceptions.DatabaseException;
 import com.miguelm.crudcliente.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -26,4 +27,14 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
--- backend/src/main/java/com/miguelm/crudcliente/services/ClientService.java
+++ backend/src/main/java/com/miguelm/crudcliente/services/ClientService.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.miguelm.crudcliente.dto.ClientDTO;
 import com.miguelm.crudcliente.entities.Client;
 import com.miguelm.crudcliente.repositories.ClientRepository;
+import com.miguelm.crudcliente.services.exceptions.DatabaseException;
 import com.miguelm.crudcliente.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,6 +37,51 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		ClientDTO dto = new ClientDTO(entity);
 		return dto;
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO objDTO) {
+		Client client = new Client();
+		client.setName(objDTO.getName());
+		client.setCpf(objDTO.getCpf());
+		client.setIncome(objDTO.getIncome());
+		client.setBirthDate(objDTO.getBirthDate());
+		client.setChildren(objDTO.getChildren());
+		client = repository.save(client);
+		objDTO = new ClientDTO(client);
+		return objDTO;
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO objDTO) {
+		Client client = new Client();
+		try {
+			client = repository.getOne(id);
+			client.setName(objDTO.getName());
+			client.setCpf(objDTO.getCpf());
+			client.setIncome(objDTO.getIncome());
+			client.setBirthDate(objDTO.getBirthDate());
+			client.setChildren(objDTO.getChildren());
+			client = repository.save(client);
+			objDTO = new ClientDTO(client);
+			return objDTO;
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}	
 	}
 
 }
